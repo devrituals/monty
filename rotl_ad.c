678729bd5711af6a366e7ee4b1a5e5d5e49ed0f0
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-09-22 01:27:14 +0100

    Update

--- rotl_ad.c
+++ rotl_ad.c
@@ -3,6 +3,7 @@
   *f_rotl- rotates the stack to the top
   *@head: stack head
   *@counter: line_number
+  * # AUTHOR : ABDELBAR AD
   *Return: no return
  */
 void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
